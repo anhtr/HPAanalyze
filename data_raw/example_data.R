9ae741a80b03c58dccfe941a2d09ccce83e31e42
Anh Tran <trannhatanh89@gmail.com>
2020-11-24 11:50:29 -0600

    Update the built-in dataset to v20

--- data_raw/example_data.R
+++ data_raw/example_data.R
@@ -1,9 +1,10 @@
 library(HPAanalyze)
 hpa_histology_data <- hpaDownload(downloadList = "histology",
-                                  version = "v19")
+                                  version = "v20")
 
-hpa_histology_data$metadata <- list(HPAversion = "19",
+hpa_histology_data$metadata <- list(HPAversion = "20",
                                     date = Sys.time(),
-                                    packageVersion = "1.5.2")
+                                    packageVersion = "1.9.1")
 
 usethis::use_data(hpa_histology_data, internal = FALSE, overwrite = FALSE)
+
