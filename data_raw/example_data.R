d8cc771d668080f9dc9552c3d855eb24bf49ecce
Anh N Tran <trannhatanh89@gmail.com>
2021-12-03 18:20:16 -0600

    Update built-in data to version 21.0

--- data_raw/example_data.R
+++ data_raw/example_data.R
@@ -1,9 +1,10 @@
 library(HPAanalyze)
 hpa_histology_data <- hpaDownload(downloadList = "histology")
 
-hpa_histology_data$metadata <- list(HPAversion = "20.1",
+hpa_histology_data$metadata <- list(HPAversion = "21.0",
+                                    Ensemblversion = "103.38",
                                     date = Sys.time(),
-                                    packageVersion = "1.9.1")
+                                    packageVersion = "1.13.0")
 
 usethis::use_data(hpa_histology_data, internal = FALSE, overwrite = TRUE)
 
