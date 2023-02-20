8b4530664fb4dd8f5cdce7260fd9be2d0604bfc6
Anh N Tran <trannhatanh89@gmail.com>
2023-02-20 16:04:12 -0600

    1.17.1 Update internal data to v22.0

--- data_raw/example_data.R
+++ data_raw/example_data.R
@@ -1,10 +1,10 @@
 library(HPAanalyze)
 hpa_histology_data <- hpaDownload(downloadList = "histology")
 
-hpa_histology_data$metadata <- list(HPAversion = "21.1",
+hpa_histology_data$metadata <- list(HPAversion = "22.0",
                                     Ensemblversion = "103.38",
                                     date = Sys.time(),
-                                    packageVersion = "1.15.0")
+                                    packageVersion = "1.16.0")
 
 usethis::use_data(hpa_histology_data, internal = FALSE, overwrite = TRUE)
 
