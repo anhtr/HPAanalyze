70550510953df133da02999d8f11f2ac8dfb4c77
Anh N Tran <trannhatanh89@gmail.com>
2023-08-17 17:14:13 -0500

    functional hpaDownload with the new built-in table

--- R/download.R
+++ R/download.R
@@ -103,286 +103,10 @@
 
 hpaDownload <- function(downloadList = 'histology',
                         version = 'latest') {
-    ## Create a data frame with information to filter and download datasets
-    allDatasets <- tibble(
-        datasetnames = c(
-            'Normal tissue',
-            'Pathology',
-            'Subcellular location',
-            'RNA consensus tissue',
-            'RNA HPA tissue',
-            'RNA GTEx tissue',
-            'RNA FANTOM tissue',
-            'RNA single cell type',
-            'RNA single cell type tissue cluster',
-            'RNA GTEx brain region',
-            'RNA FANTOM brain region',
-            'RNA pig brain region',
-            'RNA pig brain subregion sample',
-            'RNA mouse brain region',
-            'RNA mouse brain subregion sample',
-            'RNA Allen mouse brain region',
-            'RNA HPA immune cell',
-            'RNA HPA immune cell sample',
-            'RNA Monaco immune cell',
-            'RNA Schmiedel immune cell',
-            'RNA HPA blood cell',
-            'RNA HPA blood cell sample',
-            'RNA Monaco blood cell',
-            'RNA Schmiedel blood cell',
-            'RNA HPA cell line cancer',
-            'RNA HPA cell line',
-            'RNA TCGA cancer sample',
-            'RNA transcript tissue',
-            'RNA transcript GTEx retina',
-            'RNA transcript immune cells',
-            'RNA transcript cell line', 
-            'RNA transcript pig brain',
-            'RNA transcript mouse brain'
-        ),
-        
-        tidycols = list(
-            normal_tissue =
-                c(
-                    'ensembl',
-                    'gene',
-                    'tissue',
-                    'cell_type',
-                    'level',
-                    'reliability'
-                ),
-            
-            pathology =
-                c(
-                    'ensembl',
-                    'gene',
-                    'cancer',
-                    'high',
-                    'medium',
-                    'low',
-                    'not_detected',
-                    'prognostic_favorable',
-                    'unprognostic_favorable',
-                    'prognostic_unfavorable',
-                    'unprognostic_unfavorable'
-                ),
-            
-            subcellular_location =
-                c(
-                    'ensembl',
-                    'gene',
-                    'reliability',
-                    'main_location',
-                    'additional_location',
-                    'extracellular_location',
-                    'enhanced',
-                    'supported',
-                    'approved',
-                    'uncertain',
-                    'single_cell_var_intensity',
-                    'single_cell_var_spatial',
-                    'cell_cycle_dependency',
-                    'go_id'
-                ),
-            
-            rna_tissue_consensus =
-                c('ensembl', 'gene', 'tissue', 'nx'),
-            
-            rna_tissue_hpa =
-                c('ensembl', 'gene', 'tissue', 'tpm', 'ptpm', "nx"),
-            
-            rna_tissue_gtex =
-                c('ensembl', 'gene', 'tissue', 'tpm', 'ptpm', "nx"),
-            
-            rna_tissue_fantom =
-                c(
-                    'ensembl',
-                    'gene',
-                    'tissue',
-                    'tags_per_million',
-                    'scaled_tags_per_million',
-                    "nx"
-                ),
-            
-            rna_single_cell_type =
-                c('ensembl', 'gene', 'cell_type', 'nx'),
-            
-            rna_single_cell_type_tissue =
-                c(
-                    'ensembl',
-                    'gene',
-                    'tissue',
-                    'cluster',
-                    'cell_type',
-                    'read_count',
-                    'ptpm'
-                ),
-            
-            rna_brain_gtex =
-                c('ensembl', 'gene', 'brain_region', 'tpm', 'ptpm', 'nx'),
-            
-            rna_brain_fantom =
-                c(
-                    'ensembl',
-                    'gene',
-                    'brain_region',
-                    'tags_per_million',
-                    'scaled_tags_per_million',
-                    "nx"
-                ),
-            
-            rna_pig_brain_hpa =
-                c('ensembl', 'gene', 'brain_region', 'tpm', 'ptpm', 'nx'),
-            
-            rna_pig_brain_sample_hpa =
-                c(
-                    'ensembl',
-                    'main_region',
-                    'subregion',
-                    'animal',
-                    'tpm',
-                    'ptpm'
-                ),
-            
-            rna_mouse_brain_hpa =
-                c('ensembl', 'gene', 'brain_region', 'tpm', 'ptpm', 'nx'),
-            
-            rna_mouse_brain_sample_hpa =
-                c(
-                    'ensembl',
-                    'main_region',
-                    'subregion',
-                    'animal',
-                    'tpm',
-                    'ptpm'
-                ),
-            
-            rna_mouse_brain_allen =
-                c('ensembl', 'gene', 'brain_region', 'expression_energy'),
-            
-            rna_immune_cell =
-                c('ensembl', 'gene', 'immune_cell', 'tpm', 'ptpm', 'ntpm'),
-            
-            rna_immune_cell_sample =
-                c('sample_id', 'donor', 'immune_cell', 'ensembl', 'gene', 'tpm', 'ptpm', 'ntpm'),
-            
-            rna_immune_cell_monaco =
-                c('ensembl', 'gene', 'immune_cell', 'tpm', 'ptpm'),
-            
-            rna_immune_cell_schmiedel =
-                c('ensembl', 'gene', 'immune_cell', 'tpm'),
-            
-            rna_blood_cell =
-                c('ensembl', 'gene', 'blood_cell', 'tpm', 'ptpm', 'nx'),
-            
-            rna_blood_cell_sample =
-                c('ensembl', 'blood_cell_type', 'donor', 'ptpm', 'nx'),
-            
-            rna_blood_cell_monaco =
-                c('ensembl', 'gene', 'blood_cell', 'tpm', 'ptpm'),
-            
-            rna_blood_cell_schmiedel =
-                c('ensembl', 'gene', 'blood_cell', 'tpm'),
-            
-            rna_celline_cancer =
-                c('ensembl', 'gene', 'cancer', 'tpm', 'ptpm', "ntpm"),
-            
-            rna_celline =
-                c('ensembl', 'gene', 'cell_line', 'tpm', 'ptpm', "ntpm"),
-            
-            rna_cancer_sample =
-                c('ensembl', 'sample', 'cancer', 'fpkm'),
-            
-            transcript_rna_tissue =
-                c('ensgid', 'enstid', 'sample', 'tpm'),
-            
-            transcript_rna_gtexretina =
-                c('ensgid', 'enstid', 'sample', 'tpm'),
-            
-            transcript_rna_immunecells =
-                c('ensgid', 'enstid', 'sample', 'tpm'),
-            
-            transcript_rna_celline =
-                c('ensgid', 'enstid', 'sample', 'tpm'),
-            
-            transcript_rna_pigbrain =
-                c('ensgid', 'enstid', 'sample', 'tpm'),
-            
-            transcript_rna_mousebrain =
-                c('ensgid', 'enstid', 'sample', 'tpm')
-            
-        ),
-        
-        urls = c(
-            normal_tissue =
-                'https://www.proteinatlas.org/download/normal_tissue.tsv.zip',
-            pathology =
-                'https://www.proteinatlas.org/download/pathology.tsv.zip',
-            subcellular_location =
-                'https://www.proteinatlas.org/download/subcellular_location.tsv.zip',
-            rna_tissue_consensus =
-                'https://www.proteinatlas.org/download/rna_tissue_consensus.tsv.zip',
-            rna_tissue_hpa =
-                'https://www.proteinatlas.org/download/rna_tissue_hpa.tsv.zip',
-            rna_tissue_gtex =
-                'https://www.proteinatlas.org/download/rna_tissue_gtex.tsv.zip',
-            rna_tissue_fantom =
-                'https://www.proteinatlas.org/download/rna_tissue_fantom.tsv.zip',
-            rna_single_cell_type =
-                'https://www.proteinatlas.org/download/rna_single_cell_type.tsv.zip',
-            rna_single_cell_type_tissue =
-                'https://www.proteinatlas.org/download/rna_single_cell_type_tissue.tsv.zip',
-            rna_brain_gtex =
-                'https://www.proteinatlas.org/download/rna_brain_gtex.tsv.zip',
-            rna_brain_fantom =
-                'https://www.proteinatlas.org/download/rna_brain_fantom.tsv.zip',
-            rna_pig_brain_hpa =
-                'https://www.proteinatlas.org/download/rna_pig_brain_hpa.tsv.zip',
-            rna_pig_brain_sample_hpa =
-                'https://www.proteinatlas.org/download/rna_pig_brain_sample_hpa.tsv.zip',
-            rna_mouse_brain_hpa =
-                'https://www.proteinatlas.org/download/rna_mouse_brain_hpa.tsv.zip',
-            rna_mouse_brain_sample_hpa =
-                'https://www.proteinatlas.org/download/rna_mouse_brain_sample_hpa.tsv.zip',
-            rna_mouse_brain_allen =
-                'https://www.proteinatlas.org/download/rna_mouse_brain_allen.tsv.zip',
-            rna_immune_cell =
-                'https://www.proteinatlas.org/download/rna_immune_cell.tsv.zip',
-            rna_immune_cell_sample =
-                'https://www.proteinatlas.org/download/rna_immune_cell_sample.tsv.zip',
-            rna_immune_cell_monaco =
-                'https://www.proteinatlas.org/download/rna_immune_cell_monaco.tsv.zip',
-            rna_immune_cell_schmiedel =
-                'https://www.proteinatlas.org/download/rna_immune_cell_schmiedel.tsv.zip',
-            rna_blood_cell =
-                'https://v21.proteinatlas.org/download/rna_blood_cell.tsv.zip',
-            rna_blood_cell_sample =
-                'https://v21.proteinatlas.org/download/rna_blood_cell_sample.tsv.zip',
-            rna_blood_cell_monaco =
-                'https://v21.proteinatlas.org/download/rna_blood_cell_monaco.tsv.zip',
-            rna_blood_cell_schmiedel =
-                'https://v21.proteinatlas.org/download/rna_blood_cell_schmiedel.tsv.zip',
-            rna_celline_cancer =
-                'https://www.proteinatlas.org/download/rna_celline_cancer.tsv.zip',
-            rna_celline =
-                'https://www.proteinatlas.org/download/rna_celline.tsv.zip',
-            rna_cancer_sample =
-                'https://www.proteinatlas.org/download/rna_cancer_sample.tsv.zip',
-            transcript_rna_tissue =
-                'https://www.proteinatlas.org/download/transcript_rna_tissue.tsv.zip',
-            transcript_rna_gtexretina =
-                'https://www.proteinatlas.org/download/transcript_rna_gtexretina.tsv.zip',
-            transcript_rna_immunecells =
-                'https://www.proteinatlas.org/download/transcript_rna_immunecells.tsv.zip',
-            transcript_rna_celline =
-                'https://v21.proteinatlas.org/download/transcript_rna_celline.tsv.zip',
-            transcript_rna_pigbrain =
-                'https://www.proteinatlas.org/download/transcript_rna_pigbrain.tsv.zip',
-            transcript_rna_mousebrain =
-                'https://www.proteinatlas.org/download/transcript_rna_mousebrain.tsv.zip'
-        )
-    )
-    
+    ## set longer time out
+    op <- options(timeout = 10000)
+    on.exit(options(op))
+
     ## generate a list of item to download
     replace_shortcut <- function(x, shortcut, with) {
         x <- rep(x, 1 + (length(with) - 1)*(x == shortcut))
@@ -391,22 +115,51 @@ hpaDownload <- function(downloadList = 'histology',
     }
     
     downloadList <- downloadList %>%
-        replace_shortcut('all', allDatasets$datasetnames) %>%
-        replace_shortcut('histology', allDatasets$datasetnames[1:3]) %>%
-        replace_shortcut('rna tissue', allDatasets$datasetnames[4:7]) %>%
-        replace_shortcut('rna cell type', allDatasets$datasetnames[8:9]) %>%
-        replace_shortcut('rna brain region', allDatasets$datasetnames[10:16]) %>%
-        replace_shortcut('rna immune cell', allDatasets$datasetnames[17:20]) %>%
-        replace_shortcut('rna blood cell', allDatasets$datasetnames[21:24]) %>%
-        replace_shortcut('isoform', allDatasets$datasetnames[28:33])
-
-    ## make sure that everything are downloadable, and get downloaded once
-    # downloadList <- unique(downloadList)
-    # downloadList <- downloadList[downloadList %in% allDatasets$datasetnames]
+        replace_shortcut('all', hpa_download_list$table) %>%
+        replace_shortcut('histology', 
+                         c('Normal tissue', 
+                           'Pathology', 
+                           'Subcellular location')) %>%
+        replace_shortcut('rna tissue', 
+                         c('RNA consensus tissue', 
+                           'RNA HPA tissue', 
+                           'RNA GTEx tissue', 
+                           'RNA FANTOM tissue')) %>%
+        replace_shortcut('rna cell type', 
+                         c('RNA single cell type', 
+                           'RNA single cell type tissue cluster')) %>%
+        replace_shortcut('rna brain region', 
+                         c('RNA GTEx brain region',
+                           'RNA FANTOM brain region', 
+                           'RNA pig brain region', 
+                           'RNA pig brain subregion sample', 
+                           'RNA mouse brain region', 
+                           'RNA mouse brain subregion sample', 
+                           'RNA Allen mouse brain region')) %>%
+        replace_shortcut('rna immune cell', 
+                         c('RNA HPA immunecell', 
+                           'RNA HPA immune cell sample', 
+                           'RNA Monaco immune cell', 
+                           'RNA Schmiedel immune cell')) %>%
+        replace_shortcut('rna blood cell', 
+                         c('RNA HPA blood cell', 
+                           'RNA HPA blood cell sample', 
+                           'RNA Monaco blood cell', 
+                           'RNA Schmiedel blood cell')) %>%
+        replace_shortcut('isoform', 
+                         c('RNA transcript tissue', 
+                           'RNA transcript GTEx retina', 
+                           'RNA transcript immune cells', 
+                           'RNA transcript cell line', 
+                           'RNA transcript pig brain', 
+                           'RNA transcript mouse brain'))
     
+
     # filter the datasets to download
     downloadDatasets <-
-        filter(allDatasets, datasetnames %in% downloadList)
+        hpa_download_list %>%
+        filter(version == {{version}}) %>%
+        filter(table %in% downloadList)
     
     #initiate the list of processed data to be returned
     loadedData <- list()
@@ -420,21 +173,21 @@ hpaDownload <- function(downloadList = 'histology',
         
         downloadDatasets <- filter(
             downloadDatasets,
-            datasetnames %in% c('Normal tissue',
-                                'Pathology',
-                                'Subcellular location')
+            table %in% c('Normal tissue',
+                         'Pathology',
+                         'Subcellular location')
         )
         
-        for (i in names(downloadDatasets$urls)) {
+        for (i in names(downloadDatasets$link)) {
             loadedData[[i]] <- hpa_histology_data[[i]]
         }
         
     } else if (version == "latest") {
         ## Download the requested datasets if version is "latest"
         
-        for (i in seq_along(downloadDatasets$urls)) {
+        for (i in seq_along(downloadDatasets$link)) {
             temp <- tempfile()
-            download.file(url = downloadDatasets$urls[[i]],
+            download.file(url = downloadDatasets$link[[i]],
                           destfile = temp)
             loadedData[[i]] <- read.delim2(
                 unz(temp, unzip(temp, list = TRUE)$Name[1]),
@@ -446,7 +199,7 @@ hpaDownload <- function(downloadList = 'histology',
             )
             unlink(temp)
             
-            if (downloadDatasets$datasetnames[[i]] %in% c(
+            if (downloadDatasets$table[[i]] %in% c(
                 'RNA transcript tissue',
                 'RNA transcript GTEx retina',
                 'RNA transcript immune cells',
@@ -468,7 +221,7 @@ hpaDownload <- function(downloadList = 'histology',
             }
             
             ## assign tidy colnames
-            colnames(loadedData[[i]]) <- downloadDatasets$tidycols[[i]]
+            # colnames(loadedData[[i]]) <- downloadDatasets$tidycols[[i]]
             
         }
         
@@ -476,11 +229,10 @@ hpaDownload <- function(downloadList = 'histology',
         loadedData <- lapply(loadedData, as_tibble)
         
         names(loadedData) <-
-            downloadDatasets$urls %>%
+            downloadDatasets$link %>%
             gsub('.tsv.zip|https://www.proteinatlas.org/download/|https://v21.proteinatlas.org/download/',
                  '',
                  .)
-        
     }
     
     return(loadedData)
