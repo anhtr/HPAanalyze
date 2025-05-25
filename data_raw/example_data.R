3188ab0e61f2ced8b2fb7fcd27370d6184afb8c3
Anh N Tran <trannhatanh89@gmail.com>
2025-05-25 15:53:17 -0500

    Update download function and update built-in data

--- data_raw/example_data.R
+++ data_raw/example_data.R
@@ -1,10 +1,9 @@
 library(HPAanalyze)
 hpa_histology_data <- hpaDownload(downloadList = "histology")
 
-hpa_histology_data$metadata <- list(HPAversion = "23.0",
+hpa_histology_data$metadata <- list(HPAversion = "24.0",
                                     Ensemblversion = "109",
-                                    date = Sys.time(),
-                                    packageVersion = "1.19.1")
+                                    date = Sys.time())
 
 usethis::use_data(hpa_histology_data, internal = FALSE, overwrite = TRUE)
 
